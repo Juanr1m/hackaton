a56c8e0be06618dbf82992201bc0e09d35ae511d
Rim Karimov <rimkarimov20@gmail.com>
2020-12-09 15:43:28 +0500

    added lottie splash

--- android/app/src/main/kotlin/com/example/cactus_jobs/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cactus_jobs/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.cactus_jobs
 
+import SplashView
 import io.flutter.embedding.android.FlutterActivity
 
+import io.flutter.embedding.android.SplashScreen
+
 class MainActivity: FlutterActivity() {
+
+    
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
 }
